b3af36e50946167de0e2ed7ff422b321a963c2d3
Rmood <keremunal@yahoo.com>
2016-05-16 17:56:40 +0200

    test 2

--- src/Bobbee/Main.java
+++ src/Bobbee/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		LCD.drawString("Hello World", 0, 4);
+		LCD.drawString("Hello World 2", 0, 4);
 		Delay.msDelay(5000);
 
 	}
